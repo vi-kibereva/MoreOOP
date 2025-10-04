37fc85547bd5c97eddb579148b38e1f771908ca8
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2025-10-04 23:22:07 +0300

    fixed minor bug

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,10 @@
+import lotr.*;
+import lotr.Character;
 public class Demo {
+    private static CharacterFactory factory = new CharacterFactory();
     public static void main(String args[]) {
-        System.out.println("Hello, world!");
+        Character c1 = factory.createCharacter();
+        Character c2 = factory.createCharacter();
+        GameManager.fight(c1, c2);
     }
 }
--- src/main/java/GameManager.java
+++ src/main/java/GameManager.java
@@ -1,16 +1,18 @@
 import lotr.Character;
-import lotr.CharacterFactory;
-
 public class GameManager{
-    private static CharacterFactory factory = new CharacterFactory();
     public static void fight(Character c1, Character c2){
         String loser;
         String winner;
         System.out.println(String.format("%s and %s met in the Great Forest and soon got into a fight", c1.getClass().getSimpleName(), c2.getClass().getSimpleName()));
-        if (c1.getClass().getSimpleName() == "Hobbit" && c2.getClass().getSimpleName() == "Hobbit"){
+        if (c1.getClass().getSimpleName().equals("Hobbit") && c2.getClass().getSimpleName().equals("Hobbit")){
             System.out.println("But hobbits are nonviolent creatures");
             System.out.println("So they sat there and cried until the sun set.");
             System.out.println("The end!");
+            return;
+        }
+        if (c1.getClass().getSimpleName().equals("Elf") && c2.getClass().getSimpleName().equals("Elf")){
+            System.out.println("The elves fought for a long time and none of them won");
+            System.out.println("The end");
             return;
         }
         String name_1 = c1.getClass().getSimpleName();
@@ -39,10 +41,5 @@ public class GameManager{
         }
         System.out.println(String.format("The %s has lost, the %s has won.", loser, winner));
         System.out.println("The end.");
-    }
-    public static void main(String[] args) {
-        Character c1 = factory.createCharacter();
-        Character c2 = factory.createCharacter();
-        fight(c1, c2);
     }
 }
